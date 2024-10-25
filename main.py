e27de1c0fa1b85f27d639f4e2c2ae91494c7b5ca
Philipp Schmitz <schmxtz@gmail.com>
2024-10-25 08:45:49 +0800

    Working init

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import spacy
 
+from models.phonetizer_model import PhonetizerInput
+
+# Server init
 app = FastAPI()
 
+# NLP init
+nlp = spacy.load('de_core_news_sm')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/phonetizer")
+async def get_phonetics(words: Optional[PhonetizerInput] = None):
+    if not words.user_input:
+        return
+    doc = nlp(words.user_input)
+    for token in doc:
+        print(token.text, token.pos_, token.dep_)
+    token = ' '.join([token.text for token in doc])
+    return words
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/transcriber")
+async def get_transcription(words: str):
+    return words
+
+
+# .\.venv\Scripts\pip.exe install spacy
