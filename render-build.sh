19c422d29c4b76f8b828a35398407a4c39429662
Philipp Schmitz <schmxtz@gmail.com>
2024-10-30 00:13:29 +0800

    Changes

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -U pip setuptools wheel
-pip install -U spacy
-python -m spacy download de_core_news_sm
-pip install -U fastapi[standard]
-pip install -U jptranscription
