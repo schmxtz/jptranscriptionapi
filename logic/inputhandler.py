e83351e01c1911fbd927dc1b6f30998af1b53f40
schmxtz <56893529+schmxtz@users.noreply.github.com>
2024-11-17 10:00:31 +0800

    added number conversion and symbol support

--- logic/inputhandler.py
+++ logic/inputhandler.py
@@ -1,11 +1,9 @@
 import spacy
 from jptranscription import Katakanizer, IPATranscription
 from models.phonetizer_model import PhonetizerInput
+from num2words import num2words
+import re
 
-
-# ID for NUM POS tag
-NUM_PROPN = 92
-NUM_POS = 93
 
 PUNCT_MAPPINGS = {
     ',': '、',
@@ -14,8 +12,10 @@ PUNCT_MAPPINGS = {
     '".': '「',
     '.\'': '」',
     '\'.': '「',
-    ':': ':'
+    ':': ':',
 }
+
+DOT_REPLACE_PATTERN = (re.compile(r'(\d*)(\.)(\d*\,\d*)'), r'\1\3')
 
 
 # Convert basic user input string to a list of objects for phonetizer and katakanizer
@@ -31,7 +31,6 @@ class InputHandler:
         for i in range(len(words)):
             (word, pos) = words[i]
             words[i] += (self.phonetizer.lookup_word(word, pos),)
-        print(words)
         return words
     
     def get_katakana(self, x: PhonetizerInput):
@@ -52,22 +51,46 @@ class InputHandler:
     def _handle_input(self, x: PhonetizerInput):
         if not x.user_input:
             return
+        
+        user_input = x.user_input
+        
+        # Custom rule pre-processing
+        # Iteratively removes preceding dots in decimal number (with comma)
+        while len(user_input) != len(user_input := re.sub(DOT_REPLACE_PATTERN[0], DOT_REPLACE_PATTERN[1], user_input)): pass
+
         # NLP processing
-        doc = self.nlp(x.user_input)
+        doc = self.nlp(user_input)
         words = [(token.text, token.pos_) for token in doc]
 
-        # Custom rule processing
-        InputHandler._st_rule(words)
-        # TODO: number to word
+        # Custom rule post-processing
+        for i, (word, pos) in enumerate(words):
+            InputHandler._st_rule(words, i, word, pos)
+            InputHandler._num_rule(words, i, word, pos)
+            InputHandler._symbol_rule(words, i, word, pos)
         # TODO: kg to Kilogramm 
 
         return words
 
-    def _st_rule(words):
-        for i, (word, pos) in enumerate(words):
-            if word.lower() == 'st.':
-                if  i != 0 and words[i-1][1] == 'NUM':  # st. preceded by a number
-                    words[i] = ('Stück', pos)
-                elif i != len(words)-1 and words[i+1][1] == 'PROPN':  # st. preceded by a noun with no number before
-                    words[i] = ('Sankt', pos)
+    def _st_rule(words, i, word, pos):
+        if word.lower() == 'st.':
+            if  i != 0 and words[i-1][1] == 'NUM':  # st. preceded by a number
+                words[i] = ('Stück', pos)
+            elif i != len(words)-1 and words[i+1][1] == 'PROPN':  # st. preceded by a noun with no number before
+                words[i] = ('Sankt', pos)
+    
+    def _num_rule(words, i, word, pos):
+        if pos == 'NUM':
+            try:
+                words[i] = (num2words(word.replace(',', '.'), lang='de'), pos)
+            except:
+                pass  # number is already in text form
+
+    def _symbol_rule(words, i, word, pos):
+        match word:
+            case '€':
+                words[i] = ('Euro', pos)
+            case '$':
+                words[i] = ('Dollar', pos)
+            case '%':
+                words[i] = ('Prozent', pos)
     
