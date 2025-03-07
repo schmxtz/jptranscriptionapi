de8671a9f01154b3b4f960b7e8aac271d6b31aef
unknown <56893529+schmxtz@users.noreply.github.com>
2025-03-07 19:51:13 +0800

    api changes

--- logic/inputhandler.py
+++ logic/inputhandler.py
@@ -1,6 +1,5 @@
 import spacy
 from jptranscription import Katakanizer, IPATranscription
-from models.phonetizer_model import PhonetizerInput
 from num2words import num2words
 import re
 
@@ -26,75 +25,75 @@ class InputHandler:
         self.phonetizer = IPATranscription()
 
     
-    def get_phonetics(self, x: PhonetizerInput):
-        words = self._handle_input(x)
-        for i in range(len(words)):
-            (word, pos) = words[i]
-            words[i] += (self.phonetizer.lookup_word(word, pos),)
-        return words
+    # def get_phonetics(self, x):
+    #     words = self._handle_input(x)
+    #     for i in range(len(words)):
+    #         (word, pos) = words[i]
+    #         words[i] += (self.phonetizer.lookup_word(word, pos),)
+    #     return words
     
-    def get_katakana(self, x: PhonetizerInput):
-        words = self.get_phonetics(x)
-        quotation_counter = 0
-        for i in range(len(words)):
-            (word, pos, ipa) = words[i]
-            try:
-                if pos == 'PUNCT':
-                    if word == '\"':
-                        transcription = PUNCT_MAPPINGS['.'+word] if quotation_counter % 2 == 0 else PUNCT_MAPPINGS[word+'.'] 
-                        quotation_counter += 1
-                    transcription = PUNCT_MAPPINGS.get(word) if PUNCT_MAPPINGS.get(word) else word
-                else:
-                    transcription = self.katakanizer.transcribe_word(ipa)
-                words[i] += (transcription,)
-            except Exception as e:
-                print(e)
-                words[i] += ('',)
-        return words
+    # def get_katakana(self, x: PhonetizerInput):
+    #     words = self.get_phonetics(x)
+    #     quotation_counter = 0
+    #     for i in range(len(words)):
+    #         (word, pos, ipa) = words[i]
+    #         try:
+    #             if pos == 'PUNCT':
+    #                 if word == '\"':
+    #                     transcription = PUNCT_MAPPINGS['.'+word] if quotation_counter % 2 == 0 else PUNCT_MAPPINGS[word+'.'] 
+    #                     quotation_counter += 1
+    #                 transcription = PUNCT_MAPPINGS.get(word) if PUNCT_MAPPINGS.get(word) else word
+    #             else:
+    #                 transcription = self.katakanizer.transcribe_word(ipa)
+    #             words[i] += (transcription,)
+    #         except Exception as e:
+    #             print(e)
+    #             words[i] += ('',)
+    #     return words
 
-    def _handle_input(self, x: PhonetizerInput):
-        if not x.user_input:
-            return
+    # def _handle_input(self, x: PhonetizerInput):
+    #     if not x.user_input:
+    #         return
         
-        user_input = x.user_input
+    #     user_input = x.user_input
         
-        # Custom rule pre-processing
-        # Iteratively removes preceding dots in decimal number (with comma)
-        while len(user_input) != len(user_input := re.sub(DOT_REPLACE_PATTERN[0], DOT_REPLACE_PATTERN[1], user_input)): pass
+    #     # Custom rule pre-processing
+    #     # Iteratively removes preceding dots in decimal number (with comma)
+    #     while len(user_input) != len(user_input := re.sub(DOT_REPLACE_PATTERN[0], DOT_REPLACE_PATTERN[1], user_input)): pass
 
-        # NLP processing
-        doc = self.nlp(user_input)
-        words = [(token.text, token.pos_) for token in doc]
+    #     # NLP processing
+    #     doc = self.nlp(user_input)
+    #     words = [(token.text, token.pos_) for token in doc]
 
-        # Custom rule post-processing
-        for i, (word, pos) in enumerate(words):
-            InputHandler._st_rule(words, i, word, pos)
-            InputHandler._num_rule(words, i, word, pos)
-            InputHandler._symbol_rule(words, i, word, pos)
-        # TODO: kg to Kilogramm 
+    #     # Custom rule post-processing
+    #     for i, (word, pos) in enumerate(words):
+    #         InputHandler._st_rule(words, i, word, pos)
+    #         InputHandler._num_rule(words, i, word, pos)
+    #         InputHandler._symbol_rule(words, i, word, pos)
+    #     # TODO: kg to Kilogramm 
 
-        return words
+    #     return words
 
-    def _st_rule(words, i, word, pos):
-        if word.lower() == 'st.':
-            if  i != 0 and words[i-1][1] == 'NUM':  # st. preceded by a number
-                words[i] = ('Stück', pos)
-            elif i != len(words)-1 and words[i+1][1] == 'PROPN':  # st. preceded by a noun with no number before
-                words[i] = ('Sankt', pos)
+    # def _st_rule(words, i, word, pos):
+    #     if word.lower() == 'st.':
+    #         if  i != 0 and words[i-1][1] == 'NUM':  # st. preceded by a number
+    #             words[i] = ('Stück', pos)
+    #         elif i != len(words)-1 and words[i+1][1] == 'PROPN':  # st. preceded by a noun with no number before
+    #             words[i] = ('Sankt', pos)
     
-    def _num_rule(words, i, word, pos):
-        if pos == 'NUM':
-            try:
-                words[i] = (num2words(word.replace(',', '.'), lang='de'), pos)
-            except:
-                pass  # number is already in text form
+    # def _num_rule(words, i, word, pos):
+    #     if pos == 'NUM':
+    #         try:
+    #             words[i] = (num2words(word.replace(',', '.'), lang='de'), pos)
+    #         except:
+    #             pass  # number is already in text form
 
-    def _symbol_rule(words, i, word, pos):
-        match word:
-            case '€':
-                words[i] = ('Euro', pos)
-            case '$':
-                words[i] = ('Dollar', pos)
-            case '%':
-                words[i] = ('Prozent', pos)
+    # def _symbol_rule(words, i, word, pos):
+    #     match word:
+    #         case '€':
+    #             words[i] = ('Euro', pos)
+    #         case '$':
+    #             words[i] = ('Dollar', pos)
+    #         case '%':
+    #             words[i] = ('Prozent', pos)
     
--- main.py
+++ main.py
@@ -39,20 +39,23 @@ def translate(engine, source, target, text):
 async def post_transcriblate(user_input: Optional[models.InputModel] = None):
     if not user_input: return
     if not user_input.input_text: return
+    return user_input
 
 
 
-@app.post("/transcriber")
-async def get_transcription(words: Optional[models.PhonetizerInput] = None):
-    words = input_handler.get_katakana(words)
-    return words
 
-@app.post("/translate")
-async def get_translation(translate_input: Optional[models.InputModel] = None):
-    user_input = translate_input.user_input
-    src = translate_input.language_src
-    target = translate_input.language_target
-    if user_input and src in supported_lang and target in supported_lang and src != target:
-        return GoogleTranslator(source='auto', target=target).translate(user_input)
-    return ''
+
+# @app.post("/transcriber")
+# async def get_transcription(words: Optional[models.PhonetizerInput] = None):
+#     words = input_handler.get_katakana(words)
+#     return words
+
+# @app.post("/translate")
+# async def get_translation(translate_input: Optional[models.InputModel] = None):
+#     user_input = translate_input.user_input
+#     src = translate_input.language_src
+#     target = translate_input.language_target
+#     if user_input and src in supported_lang and target in supported_lang and src != target:
+#         return GoogleTranslator(source='auto', target=target).translate(user_input)
+#     return ''
         
